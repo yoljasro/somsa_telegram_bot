a7b362760c2436eba699841310ca447adcef8ee6
saidaliyevjasur450@gmail.com <saidaliyevjasur450@gmail.com>
2023-06-25 19:28:26 +0500

    adminbro added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const TelegramBot = require('node-telegram-bot-api');
-const mongoose = require('mongoose');
+const TelegramBot = require("node-telegram-bot-api");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+let port = process.env.PORT || 5000;
+// admin-bro
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("admin-bro-expressjs");
+const AdminBroMongoose = require("admin-bro-mongoose");
 
 // MongoDBga ulanish
-mongoose.connect('mongodb://localhost:27017/somsa_bot', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/telegram_bot", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDBga ulanishda xatolik:'));
-db.once('open', function() {
-  console.log('MongoDBga ulanildi');
+db.on("error", console.error.bind(console, "MongoDBga ulanishda xatolik:"));
+db.once("open", function () {
+  console.log("MongoDBga ulanildi");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world. I'm JasurBek");
+});
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port https://localhost:${port}`);
 });
 
 // Foydalanuvchi ma'lumotlari uchun User skemasi
@@ -16,89 +34,93 @@ const userSchema = new mongoose.Schema({
   address: String,
   order: {
     type: String,
-    enum: ['go\'shtli quti', 'tovuqli quti', 'sabzavotli quti', 'mix quti']
+    enum: ["go'shtli quti", "tovuqli quti", "sabzavotli quti", "mix quti"],
   },
   quantity: Number,
-  price: Number
+  price: Number,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Telegram botni yaratish
-const token = '6053180396:AAGZIuR8m80yyY92hp_ThWde-n3PXe8Wb94';
-const bot = new TelegramBot(token, {polling: true});
+const token = "6053180396:AAGZIuR8m80yyY92hp_ThWde-n3PXe8Wb94";
+const bot = new TelegramBot(token, { polling: true });
 
-// Qutilar va somsalarning narxlari
-const prices = {
-  'go\'shtli quti': 40000,
-  'tovuqli quti ': 35000,
-  'sabzavotli quti': 30000,
-  'mix quti': 37000
+// Ro'yxatdan o'tish buttoni
+const registerKeyboard = {
+  keyboard: [[{ text: "Ro'yxatdan o'tish", request_contact: true }]],
+  resize_keyboard: true,
+  one_time_keyboard: true,
 };
 
-// Foydalanuvchi ismini kiritish uchun buyruq
+// Foydalanuvchiga bot haqida ma'lumot berish
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Assalomu alaykum! Ismingizni kiriting:');
-});
-
-// Foydalanuvchi ma'lumotlarini MongoDBga saqlash
-bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  const message =
+    "Assalomu alaykum! Bu somsa buyurtma boti. Siz buyurtma berish uchun qulay interfeys tajribasi yasashingiz mumkin. Buyurtma berish uchun ro'yxatdan o'tishingiz kerak.\n\nRo'yxatdan o'tish tugmasini bosing:";
+  bot.sendMessage(chatId, message, { reply_markup: registerKeyboard });
 
-  // Foydalanuvchi ismini saqlash
-  if (!msg.text.startsWith('/')) {
-    const name = msg.text;
+  // Foydalanuvchidan ismini, telefon raqamini va manzilini so'ralish
+  bot.on("contact", async (msg) => {
+    const chatId = msg.chat.id;
 
-    // Raqamni kiritish uchun buyruq
-    bot.sendMessage(chatId, `Telefon raqamingizni kiriting:`);
+    bot.sendMessage(chatId, "Ismingizni kiriting:");
+    const name = await new Promise((resolve) => {
+      bot.once("message", (msg) => {
+        resolve(msg.text);
+      });
+    });
+
+    bot.sendMessage(
+      chatId,
+      "Bog'lanishimiz uchun telefon raqamingizni kiriting:"
+    );
     const phoneNumber = await new Promise((resolve) => {
-      bot.once('message', (msg) => {
+      bot.once("message", (msg) => {
         resolve(msg.text);
       });
     });
 
-    // Manzilni kiritish uchun buyruq
-    bot.sendMessage(chatId, 'Manzilingizni kiriting:');
+    bot.sendMessage(chatId, "Manzilingizni kiriting:");
     const address = await new Promise((resolve) => {
-      bot.once('message', (msg) => {
+      bot.once("message", (msg) => {
         resolve(msg.text);
       });
     });
+
+    // Qutilar va somsalarning narxlari
+    const prices = {
+      "go'shtli quti": 40000,
+      "tovuqli quti": 35000,
+      "sabzavotli quti": 30000,
+      "mix quti": 37000,
+    };
 
     // Qutilarni ko'rsatish uchun buyruq
-    bot.sendMessage(chatId, 'Qanday turdagi somsa buyurtma qilmoqchisiz?', {
+    bot.sendMessage(chatId, "Qanday turdagi somsa buyurtma qilmoqchisiz?", {
       reply_markup: {
         inline_keyboard: [
           [
-            {
-              text: 'Go\'shtli quti',
-              callback_data: 'go\'shli'
-            },
-            {
-              text: 'Tovuqli quti',
-              callback_data: 'tovuqli'
-            },
-            {
-              text: 'Sabzavotli quti',
-              callback_data: 'sabzavotli'
-            },
-            {
-              text: 'Aralash quti',
-              callback_data: 'aralash'
-            }
-          ]
-        ]
-      }
+            { text: "Go'shtli quti", callback_data: "go'shtli quti" },
+            { text: "Tovuqli quti", callback_data: "tovuqli quti" },
+            { text: "Sabzavotli quti", callback_data: "sabzavotli quti" },
+            { text: "Mix quti", callback_data: "mix quti" },
+          ],
+        ],
+      },
     });
 
     // Foydalanuvchining buyurtmasini qabul qilish
-    bot.on('callback_query', async (query) => {
+    bot.on("callback_query", async (query) => {
       const order = query.data;
 
       // Qancha somsa kerakligini so'ralish
-      bot.sendMessage(chatId, `Nechta quti  somsa buyurtma berasiz? Har bir quti  Narxi ${prices[order]} so'm: `);
+      bot.sendMessage(
+        chatId,
+        `Nechta quti somsa buyurtma berasiz? Har bir quti narxi ${prices[order]} so'm: `
+      );
       const quantity = await new Promise((resolve) => {
-        bot.once('message', (msg) => {
+        bot.once("message", (msg) => {
           resolve(parseInt(msg.text));
         });
       });
@@ -111,17 +133,76 @@ bot.on('message', async (msg) => {
         address: address,
         order: order,
         quantity: quantity,
-        price: price
+        price: price,
+      });
+
+      bot.on("message", async (msg) => {
+        if (msg.text === "/start") {
+          bot.sendMessage(
+            chatId,
+            `Buyurtmangiz qabul qilindi. Umumiy narx ${price} so'm. Iltimos, boshqa buyurtma bermoqchi bo'lsangiz /start buyrug'ini bosing.`
+          );
+          bot.removeListeners("message");
+          bot.removeListeners("callback_query");
+        }
       });
 
       try {
         const savedUser = await user.save();
+        bot.sendMessage(
+          chatId,
+          `Buyurtmangiz qabul qilindi. Umumiy narx ${price} so'm. Iltimos, boshqa buyurtma bermoqchi bo'lsangiz /start buyrug'ini bosing.`
+        );
         console.log(savedUser);
-        bot.sendMessage(chatId, `Buyurtmangiz qabul qilindi. Umumiy narx  ${prices[order] * quantity} so\'m. Iltimos, boshqa buyurtma bermoqchi bo\'lsangiz /start buyrug\'ini bosing.`);
       } catch (err) {
         console.error(err);
-        bot.sendMessage(chatId, 'Buyurtmani qabul qilishda xatolik yuzaga keldi. Iltimos, qayta urinib ko\'ring.');
+        bot.sendMessage(
+          chatId,
+          "Buyurtmani qabul qilishda xatolik yuzaga keldi. Iltimos, qayta urinib ko'ring."
+        );
       }
     });
-  }
+  });
 });
+
+// admin-bro options
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const adminBro = new AdminBro({
+  branding: {
+    companyName: "JasurBek",
+  },
+  databases: [mongoose],
+  rootPath: "/admin",
+  resources: [
+    {
+      resource: User,
+      options: {
+        parent: {
+          name: "Somsa bot orders",
+          icon: "fas fa-cogs",
+        },
+        resource: ["pathname"],
+      },
+    },
+  ],
+});
+
+const ADMIN = {
+  email: process.env.ADMIN_EMAIL || "saidaliyevjasur450@gmail.com",
+  password: process.env.ADMIN_PASSWORD || "worldhalal1221",
+};
+
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+  cookieName: process.env.ADMIN_COOKIE_NAME || "worldhalal",
+  cookiePassword: process.env.ADMIN_COOKIE_PASS || "worldhalal1221",
+  authenticate: async (email, password) => {
+    if (email === ADMIN.email && password === ADMIN.password) {
+      return ADMIN;
+    }
+    return null;
+  },
+});
+
+// app uses
+app.use("/admin", router);
